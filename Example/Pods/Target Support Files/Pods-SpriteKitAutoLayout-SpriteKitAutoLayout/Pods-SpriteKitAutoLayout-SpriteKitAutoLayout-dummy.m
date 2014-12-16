95d024be2768ed08e752ce64fea87121f3ed6dc2
maksym.grebenets <maksym.grebenets@fairfaxmedia.com.au>
2014-12-17 10:08:29 +1100

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-SpriteKitAutoLayout-SpriteKitAutoLayout/Pods-SpriteKitAutoLayout-SpriteKitAutoLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpriteKitAutoLayout_SpriteKitAutoLayout : NSObject
+@end
+@implementation PodsDummy_Pods_SpriteKitAutoLayout_SpriteKitAutoLayout
+@end
---
+++ Example/Pods/Target Support Files/Pods-Tests-SpriteKitAutoLayout/Pods-Tests-SpriteKitAutoLayout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tests_SpriteKitAutoLayout : NSObject
+@end
+@implementation PodsDummy_Pods_Tests_SpriteKitAutoLayout
+@end
